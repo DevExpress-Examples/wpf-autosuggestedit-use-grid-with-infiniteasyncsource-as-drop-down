e493cc63b2d5a896b04774201bf5c250c648374f
ivansupport <yakovlev.ivan@corp.devexpress.com>
2019-05-28 14:07:54 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Data;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.Grid;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InfiniteAsyncSourceODataSample {
+    public partial class MainWindow : ThemedWindow {
+        static string[] Properties = new string[] { "Subject", "TechnologyName", "ProductName" };
+        static char[] Separators = new char[] { ' ', ',', ';' };
+
+        static FetchRowsResult FetchRows(FetchRowsAsyncEventArgs e) {
+            if (ReferenceEquals(e.Filter, null))
+                return new FetchRowsResult(null, false);
+            var queryable = GetIssueDataQueryable().Where(MakeFilterExpression(e.Filter));
+            return queryable
+                .Skip(e.Skip)
+                .Take(42)
+                .ToArray();
+        }
+        static Expression<Func<SCIssuesDemo, bool>> MakeFilterExpression(CriteriaOperator filter) {
+            var converter = new GridFilterCriteriaToExpressionConverter<SCIssuesDemo>();
+            return converter.Convert(filter);
+        }
+        static IQueryable<SCIssuesDemo> GetIssueDataQueryable() {
+            var context = new SCEntities(new Uri("https://demos.devexpress.com/Services/WcfLinqSC/WcfSCService.svc/"));
+            return context.SCIssuesDemo;
+        }
+        static CriteriaOperator GetFilter(string text) {
+            if (string.IsNullOrEmpty(text))
+                return null;
+            var values = text.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+            if (values.Length == 0)
+                return null;
+            return new GroupOperator(GroupOperatorType.Or, Properties.SelectMany(prov => values.Select(val => new FunctionOperator(FunctionOperatorType.Contains, new OperandProperty(prov), new OperandValue(val)))));
+        }
+        static void AssignFilter(AutoSuggestEdit editor, string searchString) {
+            if (editor.PopupElement is GridControl grid)
+                grid.FixedFilter = GetFilter(searchString);
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+            var source = new InfiniteAsyncSource() { ElementType = typeof(SCIssuesDemo) };
+            source.FetchRows += (o, e) => e.Result = Task.Run(() => FetchRows(e));
+            autoSuggestEdit.ItemsSource = source;
+            Closing += (o, e) => source.Dispose();
+        }
+
+        void QuerySubmitted(object sender, AutoSuggestEditQuerySubmittedEventArgs e) {
+            AssignFilter(autoSuggestEdit, e.Text);
+        }
+        void PopupOpened(object sender, RoutedEventArgs e) {
+            AssignFilter(autoSuggestEdit, autoSuggestEdit.EditText);
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,66 @@
+﻿Imports DevExpress.Data.Filtering
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Data
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Linq
+Imports System.Linq.Expressions
+Imports System.Threading.Tasks
+Imports System.Windows
+
+Namespace InfiniteAsyncSourceODataSample
+    Partial Public Class MainWindow
+        Inherits ThemedWindow
+
+        Private Shared Properties() As String = { "Subject", "TechnologyName", "ProductName" }
+        Private Shared Separators() As Char = { " "c, ","c, ";"c }
+
+        Private Shared Function FetchRows(ByVal e As FetchRowsAsyncEventArgs) As FetchRowsResult
+            If ReferenceEquals(e.Filter, Nothing) Then
+                Return New FetchRowsResult(Nothing, False)
+            End If
+            Dim queryable = GetIssueDataQueryable().Where(MakeFilterExpression(e.Filter))
+            Return queryable.Skip(e.Skip).Take(42).ToArray()
+        End Function
+        Private Shared Function MakeFilterExpression(ByVal filter As CriteriaOperator) As Expression(Of Func(Of SCIssuesDemo, Boolean))
+            Dim converter = New GridFilterCriteriaToExpressionConverter(Of SCIssuesDemo)()
+            Return converter.Convert(filter)
+        End Function
+        Private Shared Function GetIssueDataQueryable() As IQueryable(Of SCIssuesDemo)
+            Dim context = New SCEntities(New Uri("https://demos.devexpress.com/Services/WcfLinqSC/WcfSCService.svc/"))
+            Return context.SCIssuesDemo
+        End Function
+        Private Shared Function GetFilter(ByVal text As String) As CriteriaOperator
+            If String.IsNullOrEmpty(text) Then
+                Return Nothing
+            End If
+            Dim values = text.Split(Separators, StringSplitOptions.RemoveEmptyEntries)
+            If values.Length = 0 Then
+                Return Nothing
+            End If
+            Return New GroupOperator(GroupOperatorType.Or, Properties.SelectMany(Function(prov) values.Select(Function(val) New FunctionOperator(FunctionOperatorType.Contains, New OperandProperty(prov), New OperandValue(val)))))
+        End Function
+        Private Shared Sub AssignFilter(ByVal editor As AutoSuggestEdit, ByVal searchString As String)
+            If TypeOf editor.PopupElement Is GridControl Then
+                Dim grid = DirectCast(editor.PopupElement, GridControl)
+                grid.FixedFilter = GetFilter(searchString)
+            End If
+        End Sub
+
+        Public Sub New()
+            InitializeComponent()
+            Dim source = New InfiniteAsyncSource() With {.ElementType = GetType(SCIssuesDemo)}
+            AddHandler source.FetchRows, Sub(o, e) e.Result = Task.Run(Function() FetchRows(e))
+            autoSuggestEdit.ItemsSource = source
+            AddHandler Closing, Sub(o, e) source.Dispose()
+        End Sub
+
+        Private Sub QuerySubmitted(ByVal sender As Object, ByVal e As AutoSuggestEditQuerySubmittedEventArgs)
+            AssignFilter(autoSuggestEdit, e.Text)
+        End Sub
+        Private Sub PopupOpened(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            AssignFilter(autoSuggestEdit, autoSuggestEdit.EditText)
+        End Sub
+    End Class
+End Namespace
